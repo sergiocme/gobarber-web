c7a02c04966c01498b9915c4f4423b7d9b2cd69a
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-05-16 20:00:51 -0300

    Use unform to manage state from signup form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
@@ -8,31 +9,37 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="Gobaber's logo" />
-      <form>
-        <h1>SignUp an Account</h1>
+const SignIn: React.FC = () => {
+  const handleSubmit = (data: object): void => {
+    console.log('handleSubmit: ', data);
+  };
 
-        <Input name="name" icon={FiUser} placeholder="Name" />
-        <Input name="email" icon={FiMail} placeholder="Email" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">SignUp</Button>
-      </form>
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Gobaber's logo" />
+        <Form onSubmit={handleSubmit}>
+          <h1>SignUp an Account</h1>
 
-      <a href="#newAccount">
-        <FiArrowLeft />
-        SignIn
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">SignUp</Button>
+        </Form>
+
+        <a href="#newAccount">
+          <FiArrowLeft />
+          SignIn
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
