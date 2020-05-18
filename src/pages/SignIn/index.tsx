f79be8cd77f7ab0589b465efc0ea5ca10bf71279
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-05-18 13:40:50 -0300

    Validate signin form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
@@ -8,32 +12,51 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="Gobaber's logo" />
-      <form>
-        <h1>SignIn with Gobaber Account</h1>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <Input name="email" icon={FiMail} placeholder="Email" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
-        <Button type="submit">SignIn</Button>
+  const handleSubmit = useCallback(async (data: object) => {
+    formRef.current?.setErrors({});
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().required('Email is required'),
+        password: Yup.string().required('Password is required'),
+      });
 
-        <a href="#forgotPassword">Forgot Password?</a>
-      </form>
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-      <a href="#newAccount">
-        <FiLogIn />
-        SignUp
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Gobaber's logo" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>SignIn with Gobaber Account</h1>
+
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">SignIn</Button>
+
+          <a href="#forgotPassword">Forgot Password?</a>
+        </Form>
+
+        <a href="#newAccount">
+          <FiLogIn />
+          SignUp
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
